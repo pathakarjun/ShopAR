883fbf518f34cbc73ba26fd4d012259e2d0f60a8
bradenross <bradenhross@gmail.com>
2022-04-09 13:53:21 -0500

    Working Firebase - Does not segue from login/signup to homepage.

--- shop_ar/lib/loginSignup/login_page.dart
+++ shop_ar/lib/loginSignup/login_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shop_ar/loginSignup/forgot_password.dart';
 import 'package:shop_ar/loginSignup/loading_main.dart';
 import 'package:shop_ar/loginSignup/signup_page.dart';
 
@@ -26,6 +27,8 @@ class _LoginPage extends State<LoginPage> {
         print('No user found for that email.');
       } else if (e.code == 'wrong-password') {
         print('Wrong password provided for that user.');
+      } else {
+        print(e.code);
       }
     }
   }
@@ -33,53 +36,68 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          children: [
-            Text("ShopAR"),
-            SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-              ),
-              controller: email,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Image(image: AssetImage("assets/images/Logo.png")),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                  controller: email,
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                  controller: password,
+                ),
+                SizedBox(height: 60),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
+                  ),
+                  onPressed: () {
+                    login();
+                  },
+                  child: const Text(
+                    'Log In',
+                  ),
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
+                  ),
+                  onPressed: () {
+                    print(FirebaseAuth.instance.currentUser);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupPage()),
+                    );
+                  },
+                  child: Text("Sign Up"),
+                ),
+                SizedBox(height: 15),
+                TextButton(onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ForgotPassPage()),
+                  );
+                }, child: Text("Forgot Password?")),
+              ],
             ),
-            SizedBox(height: 15),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-              controller: password,
-            ),
-            SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                login();
-              },
-              child: const Text(
-                'Home',
-              ),
-            ),
-            SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                print(FirebaseAuth.instance.currentUser);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupPage()),
-                );
-              },
-              child: Text("Sign Up"),
-            ),
-            SizedBox(height: 15),
-            TextButton(onPressed: () {}, child: Text("Forgot Password?"))
-          ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
